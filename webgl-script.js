850aa8aef7973cf177b409b3223efce27483a23f
Hassan Shaikh <thehasanshaikh@gmail.com>
2025-01-07 17:48:56 +0530

    updated paper backgroud image

--- webgl-script.js
+++ webgl-script.js
@@ -55,6 +55,10 @@ SOFTWARE.
 
 // Simulation section
 
+document.addEventListener("DOMContentLoaded", () => {
+  initializeFluidSimulation();
+});
+
 const canvas = document.getElementById("fluid-canvas");
 resizeCanvas();
 const { gl, ext } = getWebGLContext(canvas);
@@ -1164,47 +1168,47 @@ function resizeDoubleFBO(target, w, h, internalFormat, format, type, param) {
   return target;
 }
 
-function createTextureAsync(url) {
-  let texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.RGB,
-    1,
-    1,
-    0,
-    gl.RGB,
-    gl.UNSIGNED_BYTE,
-    new Uint8Array([255, 255, 255])
-  );
+// function createTextureAsync(url) {
+// 	let texture = gl.createTexture();
+// 	gl.bindTexture(gl.TEXTURE_2D, texture);
+// 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+// 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+// 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+// 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+// 	gl.texImage2D(
+// 		gl.TEXTURE_2D,
+// 		0,
+// 		gl.RGB,
+// 		1,
+// 		1,
+// 		0,
+// 		gl.RGB,
+// 		gl.UNSIGNED_BYTE,
+// 		new Uint8Array([255, 255, 255])
+// 	);
 
-  let obj = {
-    texture,
-    width: 1,
-    height: 1,
-    attach(id) {
-      gl.activeTexture(gl.TEXTURE0 + id);
-      gl.bindTexture(gl.TEXTURE_2D, texture);
-      return id;
-    },
-  };
+// 	let obj = {
+// 		texture,
+// 		width: 1,
+// 		height: 1,
+// 		attach(id) {
+// 			gl.activeTexture(gl.TEXTURE0 + id);
+// 			gl.bindTexture(gl.TEXTURE_2D, texture);
+// 			return id;
+// 		}
+// 	};
 
-  // let image = new Image();
-  // image.onload = () => {
-  // 	obj.width = image.width;
-  // 	obj.height = image.height;
-  // 	gl.bindTexture(gl.TEXTURE_2D, texture);
-  // 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-  // };
-  // image.src = url;
+// 	// let image = new Image();
+// 	// image.onload = () => {
+// 	// 	obj.width = image.width;
+// 	// 	obj.height = image.height;
+// 	// 	gl.bindTexture(gl.TEXTURE_2D, texture);
+// 	// 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+// 	// };
+// 	// image.src = url;
 
-  return obj;
-}
+// 	return obj;
+// }
 
 function updateKeywords() {
   let displayKeywords = [];
