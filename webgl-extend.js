4fd60003fcd4a245e33ee14e2e391b89be71de3a
Hassan Shaikh <thehasanshaikh@gmail.com>
2025-01-15 17:28:02 +0530

    updated the fluid effect time

--- webgl-extend.js
+++ webgl-extend.js
@@ -1,29 +1,34 @@
- // Import the fluid simulation library
- import WebGLFluidEnhanced from 'webgl-fluid-enhanced';
+// Import the fluid simulation library
+import WebGLFluidEnhanced from "webgl-fluid-enhanced";
 
- // Get the container div
- const container = document.getElementById('fluid-container');
+// Get the container div
+const container = document.getElementById("fluid-container");
 
- // Initialize the fluid simulation
- const simulation = new WebGLFluidEnhanced(container);
- simulation.start();
+// Initialize the fluid simulation
+const simulation = new WebGLFluidEnhanced(container);
+simulation.start();
 
- 
-  // Trigger a random splat every 3 seconds
+// Trigger a random splat every 3 seconds
 setInterval(() => {
-  simulation.multipleSplats(10); // 1 random splat
-}, 3000);
+  simulation.multipleSplats(15); // 1 random splat
+}, 1500);
 
+// Stop the simulation when the page is about to unload
+window.addEventListener("beforeunload", () => {
+  simulation.stop();
+});
 
-
- // Stop the simulation when the page is about to unload
- window.addEventListener('beforeunload', () => {
-   simulation.stop();
- });
-
- simulation.setConfig({
-  colorPalette: ['#120F02', '#91760D', '#BC9E22', '#D3BC60', '#E1D18F','#F0E7C4','#241E03'],
-  backgroundColor: '#B59410',
+simulation.setConfig({
+  colorPalette: [
+    "#120F02",
+    "#91760D",
+    "#BC9E22",
+    "#D3BC60",
+    "#E1D18F",
+    "#F0E7C4",
+    "#111111",
+  ],
+  backgroundColor: "#B59410",
   transparent: false,
-
+  bloom: false,
 });
